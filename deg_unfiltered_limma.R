c19bd98142560081b5b64f4b0d1ee7422aa9b689
laupierre <laupierre@users.noreply.github.com>
2023-05-05 18:25:15 +0200

    Update deg_unfiltered_limma.R

--- deg_unfiltered_limma.R
+++ deg_unfiltered_limma.R
@@ -41,11 +41,57 @@ row.names (pheno) <- pheno$sample <- colnames (a)
 pheno$genotype <- gsub ("_.*", "", pheno$sample)
 pheno$genotype [pheno$genotype == "PGBD5"] <- "shPGBD5" 
 ## remove a pair of samples
-#pheno <- pheno[!pheno$sample %in% c("PGBD5_sh8_4","shCONTROL_4", "CONTROL_4"), ]
+
+# remove all samples from batch 6
+pheno <- pheno[grep ("_6_", pheno$sample, invert=TRUE), ]
+# remove an additional sample: MsCTX_PGBD5_sh8_7_S23
+pheno <- pheno[grep ("PGBD5_sh8_7", pheno$sample, invert=TRUE), ]
 pheno
 
+a <- a[ ,colnames (a) %in% pheno$sample]
+idx <- match (colnames (a), pheno$sample)
+pheno <- pheno[idx, ]
+
+stopifnot (colnames (a) == pheno$sample)
 
 
+
+x <- DGEList(counts=a) 
+
+# filter for cpm > 6 in at least 3 samples
+isexpr <- rowSums(cpm(x) > 6) >= 3
+x <- x[isexpr, ]
+dim (x$counts)
+
+## paired limma test (the paired factor is treated as a batch factor)
+
+celltype <- factor (pheno$genotype)
+batch <- factor (gsub (".*_", "", gsub ("_S.*", "", colnames (x$counts)) ))
+batch <- factor (as.numeric (gsub ("\\..*", "", batch)))
+batch
+# Levels: 4 6 7 8
+
+design <- model.matrix (~ 0 + celltype + batch) 
+colnames (design) <- gsub ("celltype", "", colnames (design))
+
+contr.matrix <- makeContrasts (shpgbvsctrl = shPGBD5-CONTROL, shpgbdvsshctrl = shPGBD5-shCONTROL, levels = colnames(design))
+
+v <- voomWithQualityWeights(x, design=design, plot=TRUE)
+vfit <- lmFit(v, design)
+vfit <- contrasts.fit(vfit, contrasts=contr.matrix)
+efit <- eBayes(vfit, trend=TRUE)
+
+res1 <- topTable(efit,coef=1,sort.by="P", n= Inf)
+res2 <- topTable(efit,coef=2,sort.by="P", n= Inf)
+
+
+res1[row.names (res1) == anno[anno$gene_name == "Pgbd5", ]$gene_id, ]
+#                          logFC  AveExpr         t      P.Value  adj.P.Val
+#ENSMUSG00000050751.16 -1.181046 6.163766 -4.949085 0.0007322531 0.01160692
+
+res2[row.names (res2) == anno[anno$gene_name == "Pgbd5", ]$gene_id, ]
+#                          logFC  AveExpr         t      P.Value  adj.P.Val
+#ENSMUSG00000050751.16 -1.580888 6.163766 -7.013265 5.456355e-05 0.02951769
 
 
 
