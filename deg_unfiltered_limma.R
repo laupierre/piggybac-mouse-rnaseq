48975c0aa3a5adb6e14473ed74a471a1ee361fab
laupierre <laupierre@users.noreply.github.com>
2023-05-05 18:02:38 +0200

    Update and rename deg_unfiltered_analysis.R to deg_unfiltered_limma.R

---
+++ deg_unfiltered_limma.R
@@ -0,0 +1,52 @@
+## 201110_A00558_0095_BHVNWGDMXX 
+# /Volumes/king/piggybac/mouse2020
+
+library (limma)
+library (edgeR)
+library (openxlsx)
+
+
+anno <- read.delim ("gencode.vM32.annotation.txt")
+anno <- anno[ ,grep ("transcript_id", colnames (anno), invert=TRUE)]
+anno <- unique (anno)
+
+a <- read.delim ("subread.counts.txt", skip=1)
+a <- a[ ,grep ("ene|bam", colnames (a))]
+a <- a[grep ("miRNA|Mt_tRNA|Mt_rRNA|rRNA|snRNA|snoRNA|scRNA|sRNA|misc_RNA|scaRNA|ribozyme|IG_|TR_", a$gene_type, invert=TRUE), ]
+colnames (a) <- gsub ("_S[0-9]+.*", "", colnames (a)) 
+colnames (a) <- gsub ("star.IIT_RNM_", "", colnames (a))
+
+a <- merge (a, anno, by.x="Geneid", by.y="gene_id", all.x=TRUE) 
+a <- a[ ,grep ("gene_type.y|gene_name.y", colnames (a), invert=TRUE)]
+colnames (a) [colnames (a) == "gene_name.x"] <- "gene_name"
+colnames (a) [colnames (a) == "gene_type.x"] <- "gene_type"
+
+#write.xlsx (a, "star_gene_raw_counts.xlsx", rowNames=F)
+
+
+annot <- a
+annot <- annot[ ,c("Geneid", "gene_name", "gene_type", "mgi_id", "external_gene_name", "description")]
+
+
+torm <- c("gene_name", "gene_type", "mgi_id", "external_gene_name", "description")
+a <- a[ ,!colnames (a) %in% torm]
+row.names (a) <- a[ ,1]
+colnames (a) <- gsub ("star.IIT_X_", "", colnames (a))
+a <- a[ ,-1]
+
+
+pheno <- data.frame (matrix (nrow=dim (a)[2], ncol=2))
+colnames (pheno) <- c("sample", "genotype")
+row.names (pheno) <- pheno$sample <- colnames (a)
+pheno$genotype <- gsub ("_.*", "", pheno$sample)
+pheno$genotype [pheno$genotype == "PGBD5"] <- "shPGBD5" 
+## remove a pair of samples
+#pheno <- pheno[!pheno$sample %in% c("PGBD5_sh8_4","shCONTROL_4", "CONTROL_4"), ]
+pheno
+
+
+
+
+
+
+
